65e81216593c9ecfe946b5677269e42868a93eb5
Vitalii27 <v.khlystun01@gmail.com>
2017-07-08 23:52:16 +0300

    html in process, add map

---
+++ app/js/map.js
@@ -0,0 +1,18 @@
+var mapGoogle = (function() {
+    var init = function() {
+        var uluru = { lat: 50.005440, lng: 36.229861 };
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 14,
+            center: uluru,
+            scrollwheel: false
+
+        });
+
+    };
+    return { init: init };
+
+})();
+
+if ($('#map').length) {
+    google.maps.event.addDomListener(window, 'load', mapGoogle.init);
+}
